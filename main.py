f043fce2a762a4fe12db684a3561f6ffef83545a
Markusssian <mareshkakrakazabr@gmail.com>
2023-03-28 18:28:07 +1000

    task2

--- main.py
+++ main.py
@@ -9,5 +9,10 @@ def index():
     return render_template("base.html", title="Шаблон")
 
 
+@app.route("/training/<speciality>")
+def training(speciality):
+    return render_template("training.html", title="Схема", speciality=speciality)
+
+
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080)
