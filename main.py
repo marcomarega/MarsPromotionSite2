61b1a6350dd00f2793605bf7bef898583fe32e19
Markusssian <mareshkakrakazabr@gmail.com>
2023-03-28 20:34:54 +1000

    task5

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "marcomaregasecretkey"
+
 specialities = [
     "инженер-исследователь",
     "пилот",
@@ -47,9 +52,25 @@ def auto_answer():
         "Профессия": "Программист",
         "Пол": "male",
         "Мотивация": "фвылорфлоафылврафафоырва",
-        "Готовы остаться на Марсе?": False
+        "Готовы остаться на Марсе?": False,
     }
     return render_template("auto_answer.html", title="Анкета", answer=answer)
+
+
+class LoginForm(FlaskForm):
+    astronaut_id = StringField("id астронавта", validators=[DataRequired()])
+    astronaut_password = PasswordField("Пароль астронавта", validators=[DataRequired()])
+    captain_id = StringField("id капитана", validators=[DataRequired()])
+    captain_password = PasswordField("Пароль капитана", validators=[DataRequired()])
+    submit = SubmitField("Войти")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect("/")
+    return render_template("login.html", title="Аварийный доступ", form=form)
 
 
 if __name__ == "__main__":
